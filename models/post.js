f772d8905d354f24f612df6187183486faa6de2c
DavidB <david.bernard7612@outlook.fr>
2024-08-05 20:14:46 +0200

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,14 +1,38 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    owner: {type: String, required: true},
-    garden: {type: String, required: true},
-    text: {type: String, required: true},
-    pictures: {type: [String], required: true},
-    replies: {type: [Number], required: true},
-    createdAt: {type: Date, default: Date.now},
-    title: {type: String, required: true},
-    likes: {type: [Object], required: true},
+    owner: {
+        type: String,
+        equired: true
+    },
+    garden: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    pictures: {
+        type: [String],
+        required: true
+    },
+    replies: {
+        type: [Number],
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: [Object],
+        required: true
+    },
 });
 
 const Post = mongoose.model('Post', postSchema);

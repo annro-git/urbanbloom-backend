adc322d912038c129c0aa19c88b66078b309ea58
DavidB <david.bernard7612@outlook.fr>
2024-08-06 08:42:29 +0200

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -17,43 +17,41 @@ router.post('/create', (req, res) => {
         likes
     })
     post.save()
-        .then(data => res.json({result: true, data}))
-        .catch(error => res.json({message: error}))
+        .then(data => res.json({ result: true, data }))
+        .catch(error => res.json({ message: error }))
 });
 
 router.get('/all', (req, res) => {
     Post.find()
-        .then(data => res.json({result: true, data}))
-        .catch(error => res.json({message: error}))
+        .then(data => res.json({ result: true, data }))
+        .catch(error => res.json({ message: error }))
 });
 
 router.get('/search/:owner', (req, res) => {
     Post.find(req.params.owner)
-        .then(data => res.json({result: true, data}))
-        .catch(error => res.json({message: error}))
+        .then(data => res.json({ result: true, data }))
+        .catch(error => res.json({ message: error }))
 });
 
 router.get('/search/:title', (req, res) => {
-    Post.find({title: req.params.title})
-        .then(data => res.json({result: true, data}))
-        .catch(error => res.json({message: error}))
+    Post.find({ title: req.params.title })
+        .then(data => res.json({ result: true, data }))
+        .catch(error => res.json({ message: error }))
 });
 
 router.put('/update/:owner/:title', (req, res) => {
     const { owner, title } = req.params
-    Post.findOneAndUpdate({owner, title}, {likes: req.body.likes})
-        .then(data => res.json({result: true, data}))
-        .catch(error => res.json({message: error}))
+    Post.findOneAndUpdate({ owner, title }, { likes: req.body.likes })
+        .then(data => res.json({ result: true, data }))
+        .catch(error => res.json({ message: error }))
 });
 
 router.put('update')
 router.delete('/delete/:title', (req, res) => {
-    Post.findOneAndDelete({title: req.params.title})
-        .then(data => res.json({result: true, data}))
-        .catch(error => res.json({message: error}))
+    Post.findOneAndDelete({ title: req.params.title })
+        .then(data => res.json({ result: true, data }))
+        .catch(error => res.json({ message: error }))
 });
-
-
 
 
 module.exports = router

1397af9d74e345553c3d81d82ff0e06094690522
DavidB <david.bernard7612@outlook.fr>
2024-08-06 10:26:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 require('./models/connection')
 
+require('./models/connection')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/user')
+
 const cors = require('cors')
 const app = express()
 
@@ -16,7 +19,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/', indexRouter)
-app.use('/user/', userRouter)
+app.use('/user', userRouter)
 
 module.exports = app

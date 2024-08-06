abfc46bad3b084d6c04e253e30c95e89a148a443
annro <42659227+annro-git@users.noreply.github.com>
2024-08-06 22:56:10 +0200

    "create event" route added

--- routes/garden.js
+++ routes/garden.js
@@ -57,7 +57,7 @@ router.post('/', async (req, res) => {
 
 })
 
-/* Create a post */
+/* Create a Post */
 router.post('/:id/post/', async (req, res) => {
     const { id } = req.params
     const { token, title, text, pictures } = req.body
@@ -90,6 +90,7 @@ router.post('/:id/post/', async (req, res) => {
     if(!isMember){
         res.status(403)
         res.json({ result: false, error: 'User is not a member'})
+        return
     }
 
     const newPost = {
@@ -102,6 +103,61 @@ router.post('/:id/post/', async (req, res) => {
         await Garden.updateOne({ _id: id }, { $push: { posts: newPost }})
         res.status(201)
         res.json({ result: true, message: 'Post created'})
+    } catch (error) {
+        res.status(400)
+        res.json({ result: false, error })
+        return
+    }
+
+})
+
+/* Create an Event */
+router.post('/:id/event/', async (req, res) => {
+    const { id } = req.params
+    const { token, title, text, pictures, date } = req.body
+
+    // Error 400 if value is missing
+    if(checkReq([id, token, title, text, pictures, date])){
+        res.status(400)
+        res.json({ result: false, error: 'Missing or empty fields'})
+        return
+    }
+
+    // Error 404 if garden doesn't exist
+    const isGarden = await Garden.findById(id)
+    if(!isGarden){
+        res.status(404)
+        res.json({ result: false, error: 'Garden not found' })
+        return
+    }
+
+    // Error 404 if user doesn't exist
+    const postOwner = await User.findOne({ token })
+    if(!postOwner){
+        res.status(404)
+        res.json({ result: false, error: 'User not found' })
+        return
+    }
+
+    // Error 403 if user is not a garden member
+    const isMember = await Garden.findOne({ members: postOwner._id })
+    if(!isMember){
+        res.status(403)
+        res.json({ result: false, error: 'User is not a member'})
+        return
+    }
+
+    const newEvent = {
+        owner: isMember._id,
+        title,
+        text,
+        pictures,
+        date: new Date(),
+    }
+    try {
+        await Garden.updateOne({ _id: id }, { $push: { events: newEvent }})
+        res.status(201)
+        res.json({ result: true, message: 'Event created'})
     } catch (error) {
         res.status(400)
         res.json({ result: false, error })

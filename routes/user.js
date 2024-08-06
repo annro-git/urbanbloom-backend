8927b7047f6fb236296641f11ae0a9d81fe04e03
annro <42659227+annro-git@users.noreply.github.com>
2024-08-06 08:57:38 +0200

    "delete user" route added

--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,14 @@ const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 const router = express.Router()
 
-const checkReq = (keys) => !keys.some(e => !e)
+const checkReq = (keys) => keys.some(e => !e)
 
-/* Create User */
+/* Create an User */
 router.post('/', async (req, res) => {
     const { email, password, firstname, lastname} = req.body
 
     // Error 400 if email or password aren't filled
-    if(!checkReq([email, password])){
+    if(checkReq([email, password])){
         res.status(400)
         res.json({ result: false, error: 'Missing or empty fields'})
         return
@@ -25,9 +25,10 @@ router.post('/', async (req, res) => {
         return
     }
 
+    const hash = bcrypt.hashSync(password, 10)
     const newUser = new User({
         email,
-        password,
+        password: hash,
         firstname,
         lastname,
         token: uid2(32),
@@ -38,11 +39,34 @@ router.post('/', async (req, res) => {
         res.status(201)
         res.json({ result: true, token: newUser.token, message: `User ${newUser.email} created` })
     } catch (error) {
-        // Error 400 if model datas aren't validate
+        // Error 400 if model datas aren't validated
         res.status(400)
         res.json({ result: false, error})
         return
     }
+})
+
+/* Delete an User by token */
+router.delete('/:token', async (req, res) => {
+    const { token } = req.params
+
+    // Error 400 if email is missing
+    if(checkReq([token])){
+        res.status(400)
+        res.json({ result: false, error: 'Missing token' })
+        return
+    }
+
+    const response = await User.findOneAndDelete({ token })
+    // Error 404 if token doesn't exist
+    if(!response){
+        res.status(404)
+        res.json({ result: false, error: 'User not found' })
+        return
+    }
+
+    res.json({ result: true, message: 'User deleted' })
+
 })
 
 module.exports = router

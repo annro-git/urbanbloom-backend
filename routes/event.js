dfdc04b9b6c80f9d7214590205e5f48b3b27a5d3
DavidB <david.bernard7612@outlook.fr>
2024-08-07 21:21:31 +0200

    Update event.js

--- routes/event.js
+++ routes/event.js
@@ -6,8 +6,6 @@ router.post('/new', (req, res) => {
 
     const { title, description, owner, garden } = req.body
     const date = Date.parse(req.body.date)
-    const createdAt = Date.now()
-    const subscribers = []
 
     if (checkReq([title, description, owner, garden, date])) {
         res.status(400)
@@ -30,33 +28,34 @@ router.post('/new', (req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 });
 
-
 router.get('/', (req, res) => {
+
     Event.find()
         .then(events => res.json(events))
         .catch(err => res.status(400).json('Error: ' + err))
+
 });
 
-
 router.get('/:id', (req, res) => {
+
     Event.findById(req.params.id)
         .then(event => res.json(event))
         .catch(err => res.status(400).json('Error: ' + err))
+
 });
 
-
 router.get('/:subscriber', (req, res) => {
+
     Event.find({ subscribers: req.params.subscriber })
         .then(events => res.json(events))
         .catch(err => res.status(400).json('Error: ' + err))
+        
 });
-
 
 router.get('/:garden', (req, res) => {
     Event.find({ garden: req.params.garden })
         .then(events => res.json(events))
         .catch(err => res.status(400).json('Error: ' + err))
 });
-
 
 module.exports = router

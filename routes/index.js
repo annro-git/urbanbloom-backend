dd801c65ebe0bef67ba1f638563b4151e2987851
annro <42659227+annro-git@users.noreply.github.com>
2024-08-22 19:49:03 +0200

    add route to post picture on cloudinary

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/users')
-const Bytescale = require('@bytescale/sdk')
-const nodefetch = require('node-fetch')
 const uid2 = require('uid2')
-
-const uploadManager = new Bytescale.UploadManager({
-  apiKey: process.env.BYTESCALE_TOKEN,
-  fetchApi: nodefetch,
-})
-
-const fileApi = new Bytescale.FileApi({
-  apiKey: process.env.BYTESCALE_TOKEN
-})
+const cloudinary = require('cloudinary').v2
 
 const { checkReq, isFound } = require('../helpers/errorHandlers')
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+  api_key: process.env.CLOUDINARY_APIKEY,
+  api_secret: process.env.CLOUDINARY_APISECRET,
+})
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.status(403).send('Forbidden')
 })
 
+/* Post picture to Cloudinary */
 router.post('/picture', async (req, res) => {
   const { token } = req.body
   const { blob } = req.files
     
   // Error 400 : Missing or empty field(s)
-  if(!checkReq([token], res)) return
+  if(!checkReq([token, blob], res)) return
   
   const user = await User.findOne({ token })
   // Error 404 : Not found
   if(!isFound('User', user, res)) return
 
-  console.log(blob)
-  const response = await uploadManager.upload({
-    data: blob.data,
-    type: blob.mime,
-    originalFileName: blob.name,
-    tags: ['from/'+user.username],
-    path: {
-      fileName: uid2(16)+'{ORIGINAL_FILE_EXT}'
-    }
-    
-  })
-  console.log(response)
-
-  res.json({ result: true })
+  cloudinary.uploader
+    .upload_stream(
+      { resource_type: 'auto' },
+      (error, result) => {
+        if(error){
+          res.status(500)
+          res.json({ result: false, error: 'Error uploading to Cloudinary'})
+          return
+        }
+        res.json({ result: true, url: result.secure_url})
+      }
+    )
+    .end(blob.data)
 })
 
 module.exports = router
